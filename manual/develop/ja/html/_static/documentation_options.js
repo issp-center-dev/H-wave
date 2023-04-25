3228393e842da6b58a179597c68600134f63b353
H-wave Developers <hwave-dev@issp.u-tokyo.ac.jp>
2023-04-25 08:22:03 +0000

    Deploy docs to develop by GitHub Actions triggered by 42222c5043c3e633a30356f0b94301480f41545a

--- manual/develop/ja/html/_static/documentation_options.js
+++ manual/develop/ja/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3-dev',
+    VERSION: '1.1-dev',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
